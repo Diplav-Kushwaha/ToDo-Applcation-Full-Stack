19c460c4e2b0b6ec6c01cabc522e5e4965069125
Diplav Kushwaha <diplavkushwaha@gmail.com>
2025-11-11 07:27:19 +0530

    Update TaskService.java

--- TaskService.java
+++ TaskService.java
@@ -1,6 +1,5 @@
 package com.diplav.ToDoApplication.services;
 
-
 import com.diplav.ToDoApplication.models.Task;
 import com.diplav.ToDoApplication.repository.TaskRepository;
 import org.springframework.stereotype.Service;
@@ -8,26 +7,40 @@ import java.util.List;
 
 @Service
 public class TaskService {
+
     private final TaskRepository taskRepository;
 
     public TaskService(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
     }
-    public List<Task> getAllTasks() {
-        return taskRepository.findAll();
-    }
-    public void createTask(String title) {
-        Task task=new Task();
-        task.setTitle(title);
-        task.setCompleted(false);
+    public void save(Task task) {
         taskRepository.save(task);
     }
     public void deleteTask(Long id) {
         taskRepository.deleteById(id);
     }
     public void toggleTask(Long id) {
-        Task task= taskRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("Invalid Task ID"));
-        task.setCompleted(!task.isCompleted());
-        taskRepository.save(task);
+        Task task = taskRepository.findById(id).orElse(null);
+        if (task != null) {
+            task.setCompleted(!task.isCompleted()); // ✅ toggle true/false
+            taskRepository.save(task);
+        }
+    }
+    public void updateTask(Long id, Task task) {
+        Task existing = taskRepository.findById(id).orElse(null);
+        if (existing != null) {
+            existing.setTitle(task.getTitle());
+            existing.setDescription(task.getDescription());
+            existing.setDate(task.getDate());
+
+            taskRepository.save(existing);
+        }
+    }
+    public List<Task> findByTitle(String title) {
+        return taskRepository.findByTitleContainingIgnoreCase(title);
+    }
+
+    public Object getAllTasks() {
+        return  taskRepository.findAll();
     }
 }
