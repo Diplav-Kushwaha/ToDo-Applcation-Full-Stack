02ac70705e32ae3f483c652f403f6df3e274e7f0
Diplav Kushwaha <diplavkushwaha@gmail.com>
2025-11-11 07:26:51 +0530

    Update TaskRepository.java

--- TaskRepository.java
+++ TaskRepository.java
@@ -3,7 +3,9 @@ package com.diplav.ToDoApplication.repository;
 import com.diplav.ToDoApplication.models.Task;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByTitleContainingIgnoreCase(String title);
 }
